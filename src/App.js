1c66c6408d140a9be06cffe68e82667817d5a2a4
RivaD2 <rivkadavidowski@gmail.com>
2020-09-12 11:41:25 -0700

    created containers for projects and bio

--- src/App.js
+++ src/App.js
@@ -8,14 +8,55 @@ function App() {
         <div class="welcome-message">Hello, Welcome To My Portfolio</div>
         </header>
       <div className="column-container">
-        <div className="column">
-          columnOne
+        <div className="column" id="column-one">
+          <div className="project-container">
+            <div className="project-image">
+              image
+            </div>
+            <div className="project-title">
+              project title
+            </div>
+            <div className="project-desc">
+              Text here
+            </div>
+          </div>
         </div>
-        <div className="column">
-          columnTwo
+        <div className="column" id="column-two">
+        <div className="project-container">
+            <div className="bio-image">
+              image
+            </div>
+            <div className="bio-title">
+              project title
+            </div>
+            <div className="bio-desc">
+              Text here
+            </div>
+          </div>
+          <div className="project-container">
+            <div className="project-image">
+              image
+            </div>
+            <div className="project-title">
+              project title
+            </div>
+            <div className="project-desc">
+              Text here
+            </div>
+          </div>
         </div>
-        <div className="column">
-          columnThree
+        <div className="column" id="column-three">
+        <div className="project-container">
+            <div className="project-image">
+              image
+            </div>
+            <div className="project-title">
+              project title
+            </div>
+            <div className="project-desc">
+              Text here
+            </div>
+          </div>
         </div>
 
 
