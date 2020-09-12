559c3de1ed8296cdec707b9e58b49e39701cf36f
RivaD2 <rivkadavidowski@gmail.com>
2020-09-12 11:27:41 -0700

    created header and columns

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div class="welcome-message">Hello, Welcome To My Portfolio</div>
+        </header>
+      <div className="column-container">
+        <div className="column">
+          columnOne
+        </div>
+        <div className="column">
+          columnTwo
+        </div>
+        <div className="column">
+          columnThree
+        </div>
+
+
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
+      
   );
 }
 
