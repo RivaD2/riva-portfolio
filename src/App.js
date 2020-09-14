144c89c182a9ed6072228d5cc7c7b94b2c687214
Riva Davidowski <riva@Sephiroth.local>
2020-09-13 17:09:45 -0700

    added images and text to page

--- src/App.js
+++ src/App.js
@@ -5,56 +5,70 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div class="welcome-message">Hello, Welcome To My Portfolio</div>
+          <div className="banner-text">
+            <h1>Hi there, so nice of you to check out my portfolio</h1>
+            <p>Riva Davidowski | Software Engineer</p>
+          </div>
         </header>
       <div className="column-container">
         <div className="column" id="column-one">
           <div className="project-container">
-            <div className="project-image">
-              image
+            <div className="project-image" id="raw-materials-image">
             </div>
             <div className="project-title">
-              project title
+              Raw Materials
             </div>
             <div className="project-desc">
-              Text here
+            A smooth looking, modern online shopping experience for high-end quality morning 
+            beverage products like whole leaf tea and whole bean coffee.
             </div>
           </div>
         </div>
         <div className="column" id="column-two">
         <div className="project-container">
-            <div className="bio-image">
-              image
-            </div>
+        <div className="banner-image">
+          </div>
             <div className="bio-title">
-              project title
+              About Me: 
             </div>
             <div className="bio-desc">
-              Text here
+            Hello, my name is Riva Davidowski and I’m a Software Engineer. I started learning  Russian when 
+            I was a child and since then  I’ve become fascinated with languages. 
+            On my journey to find meaningful work, I discovered a love for coding because it presents a similar 
+            language challenge. I’ve spent most of my professional career solving customer-facing issues 
+            and have acquired a diverse set of skills across various industries.
+           I approach learning with curiosity and I believe that this trait is useful because it allows 
+           me to learn new skills quickly. I’m passionate about problem-solving and approach troubleshooting 
+           with the customer’s perspective in mind. This skill along with my passion for understanding 
+           the way things work is why I would be an asset to any team.
+
             </div>
           </div>
           <div className="project-container">
-            <div className="project-image">
-              image
+            <div className="project-image" id="busMall-image">
+              
             </div>
             <div className="project-title">
-              project title
+             BusMall
             </div>
             <div className="project-desc">
-              Text here
+            The app’s purpose is to have group members choose which product, of the three displayed images,
+            that they would be most likely to purchase. I had to store, calculate, and visually display the 
+            resulting data using Chart.js.
             </div>
           </div>
         </div>
         <div className="column" id="column-three">
         <div className="project-container">
-            <div className="project-image">
-              image
+            <div className="project-image" id="movie-night-image">
+              
             </div>
             <div className="project-title">
-              project title
+             Movie Night
             </div>
             <div className="project-desc">
-              Text here
+            Movie Night makes it easy for a user to pick a movie from a 
+            recommendation list based of ratings. The project used a public API, SQL, and Heroku.
             </div>
           </div>
         </div>
