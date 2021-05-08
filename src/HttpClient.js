619e8a5f9d6fd1fdf352fbbb6f1e33c27db5f9ae
RivaD2 <rivkadavidowski@gmail.com>
2021-05-08 10:41:31 -0700

    typo removal and addition to json

--- src/HttpClient.js
+++ src/HttpClient.js
@@ -5,11 +5,11 @@ const nasaApiKey = process.env.REACT_APP_NASA_API_KEY
 axios.interceptors.request.use(
   // Interceptor callback has a config object for the request
   config => {
-    //If there is a params object use it, otherwise make a new object
+    // If there is a params object use it, otherwise make a new object
     config.params = config.params ? config.params : {}
     // This is the url of request(anytime I use axios.get the use() will be called
     const configUrl = config.url
-    /*if url of the request includes the nasaEndpoint(which is the url for  the imagesAPI)
+    /* If url of the request includes the nasaEndpoint(which is the url for  the imagesAPI)
     then I add a url param of the api key*/
     if (configUrl.includes(nasaEndpoint)) {
       config.params["api_key"] = nasaApiKey
